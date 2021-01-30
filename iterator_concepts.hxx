b25bdf381df4d15b25900d8e0109a4fdae371de3
Chris Brandt <cmb199@zips.uakron.edu>
2021-01-29 19:52:52 -0600

    Formatting interator_concepts.hxx file.

--- iterator_concepts.hxx
+++ iterator_concepts.hxx
@@ -9,17 +9,111 @@ namespace cmb {
 //
 // Iterator concepts
 
-// General requirement
+// concept indirectly_readable
+
+
+
+// concept indirectly_writable
+
+
+
+// concept weakly_incrementable
+
+
+
+// concept incrementable
+
+
+
+// concept input_or_output_iterator
+
+
+
+// concept sentinel_for
+
+
+
+// concept sized_sentinel_for
+
+
+
+// concept input_iterator
+
+
+
+// concept output_iterator
+
+
+
+// concept forward_iterator
+
+
+
+// concept bidirectional_iterator
+
+
+
+// concept random_access_iterator
+
+
+
+// concept contiguous_iterator
 
 
 
 
+//
 // Indirect callable requirements
 
+// concept indirectly_unary_invocable
 
 
 
+// concept indirectly_regular_unary_invocable
+
+
+
+// concept indirect_unary_predicate
+
+
+
+// concept indirect_binary_predicate
+
+
+
+// concept indirect_equivalence_relation
+
+
+
+// concept indirect_strict_weak_order
+
+
+
+
+//
 // Common algorithm requirements
+
+// concept indirectly_movable
+
+
+
+// concept indirectly_copyable
+
+
+
+// concept indirectly_swappable
+
+
+
+// concept indirectly_comparable
+
+
+
+// concept mergeable
+
+
+
+// concept sortable
 
 
 
