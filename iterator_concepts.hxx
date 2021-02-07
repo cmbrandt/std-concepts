587bd2c4c6f01fd9ef05cf57c41085747e320204
Chris Brandt <cmb199@zips.uakron.edu>
2021-02-06 20:21:05 -0600

    Working on iterator concepts, completed concept indirectly_readable.

--- iterator_concepts.hxx
+++ iterator_concepts.hxx
@@ -10,6 +10,26 @@ namespace cmb {
 // Iterator concepts
 
 // concept indirectly_readable
+namespace detail
+{
+  template <class In>
+    concept indirectly_readable_impl =
+      requires(In const in) {
+        typename std::iter_value_t<In>;
+        typename std::iter_reference_t<In>;
+        typename std::iter_rvalue_reference_t<In>;
+        requires cmb::same_as<std::iter_reference_t<In const>,
+                              std::iter_reference_t<In>>;
+      } and
+      cmb::common_reference_with<std::iter_reference_t<In>&&,
+                                 std::iter_value_t<In>&> and
+      cmb::common_reference_with<std::iter_rvalue_reference_t<In>&&,
+                                 std::iter_value_t<In> const&>;
+} // namespace detail
+
+template <class In>
+  concept indirectly_readable = 
+    cmb::detail::indirectly_readable_impl<std::remove_cvref_t<In>>;
 
 
 
